915ab2eb36d6d696731d49702848448a473a8a5d
tanpharamut <pharamut@kkumai.com>
2024-07-12 17:56:02 +0700

    web design.

---
+++ app.js
@@ -0,0 +1,47 @@
+let insidecard = document.querySelectorAll('.div-content');
+let sidebarLinks = document.querySelectorAll('.pd---content-inside-card.template-pages---sidebar ul li a');
+
+console.log("insidecard:", insidecard);
+console.log("sidebarLinks:", sidebarLinks);
+
+window.onscroll = () => {
+    // Check if window width is greater than 767 pixels
+    if (window.innerWidth > 767) {
+        insidecard.forEach(sec => {
+            let top = window.scrollY;
+            let offset = sec.offsetTop + 250;
+            let height = sec.offsetHeight;
+            let id = sec.getAttribute('id');
+
+            console.log("Top:", top);
+            console.log("Offset:", offset);
+            console.log("height:", height);
+            console.log("id:", id);
+
+            if (top >= offset && top < offset + height) {
+                sidebarLinks.forEach(links => {
+                    links.classList.remove('w--current');
+                    document.querySelector('a[href*=' + id + ']').classList.add('w--current');
+                });
+            };
+        });
+    } else {
+        // Remove 'w--current' class from sidebarLinks when window width is less than 767 pixels
+        sidebarLinks.forEach(links => {
+            links.classList.remove('w--current');
+        });
+    }
+};
+
+/*--------------------------- SHOW MENU ------------------------------*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    toggle.addEventListener('click', () =>{
+        nav.classList.toggle('show-menu')
+        toggle.classList.toggle('show-icon')
+    })
+}
+
+showMenu('nav-toggle','nav-menu')
