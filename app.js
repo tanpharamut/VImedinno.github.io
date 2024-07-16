eccebe2dcf532273d559be544c8e257ece3eeb33
tanpharamut <pharamut@kkumai.com>
2024-07-16 12:10:01 +0700

    design version4

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ window.onscroll = () => {
     if (window.innerWidth > 767) {
         insidecard.forEach(sec => {
             let top = window.scrollY;
-            let offset = sec.offsetTop + 1500;
+            let offset = sec.offsetTop + 1400;
             let height = sec.offsetHeight;
             let id = sec.getAttribute('id');
 
@@ -32,3 +32,16 @@ window.onscroll = () => {
         });
     }
 };
+
+/*--------------------------- SHOW MENU ------------------------------*/
+const showMenu = (toggleId, navId) =>{
+    const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId)
+
+    toggle.addEventListener('click', () =>{
+        nav.classList.toggle('show-menu')
+        toggle.classList.toggle('show-icon')
+    })
+}
+
+showMenu('nav-toggle','nav-menu')
